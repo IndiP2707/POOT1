40755658d999ba313c15e13cf11079a972252320
ibiza perez <indiraperezm2003@gmail.com>
2024-09-26 09:09:47 -0600

    impresion nombre

---
+++ mi_nombre.java
@@ -0,0 +1,12 @@
+package PROGRAMACION2;
+
+public class mi_nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String I = "indira Pérez Moo";
+		System.out.println("Indira Perez Moo" + I);
+
+	}
+
+}
